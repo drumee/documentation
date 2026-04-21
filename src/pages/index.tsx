7b6d323b2c2c9ea6e4e772ddf06f79a41ac6a44c
Somanos Sar <somanos@drumee.in>
2026-04-21 13:47:53 +0000

    WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="intro"
           >
             Drumee Documentation
           </Link>
