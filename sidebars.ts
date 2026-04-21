28c219c3963154b3ebdc79598dc1948bea4518c8
Somanos Sar <somanos@drumee.com>
2026-04-21 17:02:45 +0000

    Changed intry point

--- sidebars.ts
+++ sidebars.ts
@@ -2,7 +2,6 @@ import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 
 const sidebars: SidebarsConfig = {
   docs: [
-    "what-is-drumee",
     {
       type: "category",
       label: "Introduction",
