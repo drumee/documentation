0fba763ec099a8424820131d35dc3f3e1df69f2f
Somanos Sar <somanos@drumee.net>
2026-04-22 10:26:27 +0700

    Fine tune

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,7 +11,7 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Fully standlone, sovereign and scalable infrastructure',
+    title: 'Sovereign and scalable infrastructure',
     Svg: require("@site/static/img/logo.svg").default,
     description: (
       <>
@@ -45,7 +45,7 @@ const FeatureList: FeatureItem[] = [
 
 function Feature({title, Svg, description}: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
